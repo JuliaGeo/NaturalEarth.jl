5a3640e91011f44eec9f85b4a06bd22e84b6e66a
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-01-18 09:56:37 +0530

    Add the bathymetry function
    Co-authored-by: Haakon Ludvig Langeland Ervik <45243236+haakon-e@users.noreply.github.com>

--- src/NaturalEarth.jl
+++ src/NaturalEarth.jl
@@ -23,4 +23,31 @@ function naturalearth(name::String)
     GeoJSON.read(read(pth, String))
 end
 
+
+"""
+    bathymetry(contour::Int = 2000)
+
+Convenient acccess to ocean bathymetry datasets.
+Currently tested on: https://www.naturalearthdata.com/downloads/10m-physical-vectors/10m-bathymetry/
+The function returns a MultiPolygon describing the bathymetry at a given depth contour.
+The following depths should be available: [10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000, 1000, 200, 0]
+"""
+function bathymetry(contour::Int=2000)
+    global available_artifacts
+    # extract a list of all available bathymetry files
+    bathyfiles = filter(contains("bathymetry"), available_artifacts)
+    # Extract depth signifier from filename
+    matches = match.(r"ne_10m_.*_(\d+)", bathyfiles)
+    depths = parse.(Int, first.(getproperty.(matches, :captures)))
+    # Extract the file corresponding to the contour
+    fileind = findfirst(==(contour), depths)
+    isnothing(fileind) && error("Contour $contour not found. Available contours: $(sort(depths))")
+
+    # Open bathymetry file
+    bathyfile = bathyfiles[fileind]
+    return naturalearth(bathyfile)
+end
+
+
+
 end  # end module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "NaturalEarth.jl" begin
     # Write your tests here.
+    @testset "Bathymetry" begin
+        @test_throws "Available contours" bathymetry(10000000)
+        @test_nowarn bathymetry(4000)
+    end
 end
