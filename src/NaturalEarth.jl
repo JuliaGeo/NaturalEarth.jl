9aafdc705f8aa987b960df20c2f45a503f7215a8
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-03-27 09:26:14 +0530

    Implement the RasterDataSources.jl interface

--- src/NaturalEarth.jl
+++ src/NaturalEarth.jl
@@ -6,7 +6,8 @@ using Pkg.Artifacts
 using p7zip_jll
 using Scratch
 using Downloads
-using LightXML
+using XML
+using RasterDataSources
 
 download_cache = ""
 
@@ -15,7 +16,7 @@ const available_artifacts = collect(keys(
     Artifacts.select_downloadable_artifacts(Artifacts.find_artifacts_toml(@__FILE__); include_lazy=true)
 ))
 
-export naturalearth, bathymetry
+export naturalearth, bathymetry, NaturalEarthRaster, ne_raster
 
 """
     naturalearth(dataset_name::String)
@@ -29,7 +30,7 @@ function naturalearth(dataset_name::String)
     if dataset_name ∈ available_artifacts
         file_path = @artifact_str("$dataset_name/$dataset_name.geojson")
         return GeoJSON.read(read(file_path, String))
-    elseif dataset_name ∈ available_rasters
+    # elseif dataset_name ∈ available_rasters
     end
 
     @assert isfile(file_path) """
@@ -66,20 +67,98 @@ end
 
 # Get rasters from AWS
 
-function get_available_rasters(refresh = false)
-    scratchspace = Scratch.@get_scratch!("rasters")
-    xml_file = joinpath(scratchspace, "index.xml")
-    # refresh the XML file
-    if !isfile(xml_file) || refresh
-        download("https://naturalearth.s3.amazonaws.com/", xml_file)
-    end
-    # read the document
-    xml_doc = LightXML.parse_file(xml_file);
-    # get the root node
-    xml_root = LightXML.root(xml_doc);
-    @assert name(xml_root) == "ListBucketResult" "The format of the NaturalEarth data dictionary at Amazon AWS has changed, or the file is invalid.  Please file an issue at https://github.com/JuliaGeo/NaturalEarth.jl/issues."
-    return deepcopy(attributes_dict(xml_root))
+# function get_available_rasters(refresh = false)
+#     scratchspace = Scratch.@get_scratch!("rasters")
+#     xml_file = joinpath(scratchspace, "index.xml")
+#     # refresh the XML file
+#     if !isfile(xml_file) || refresh
+#         download("https://naturalearth.s3.amazonaws.com/", xml_file)
+#     end
+#     # read the document
+#     xml_doc = LightXML.parse_file(xml_file);
+#     # get the root node
+#     xml_root = LightXML.root(xml_doc);
+#     @assert name(xml_root) == "ListBucketResult" "The format of the NaturalEarth data dictionary at Amazon AWS has changed, or the file is invalid.  Please file an issue at https://github.com/JuliaGeo/NaturalEarth.jl/issues."
+#     return deepcopy(attributes_dict(xml_root))
+# end
+
+# function available_rasters_dict(keys)
+#     retval = Dict{String, Vector{VersionNumber}}()
+#     for key_element in keys
+#         key_str = key_element[1]
+#         endswith(key_str, "/") && continue # discard directory specs
+#         if haskey(key_str, "raster")
+#             name = splitext(basename(key_str))[1]
+#             dirs = splitpath(name)
+
+#             version = if length(dirs) == 1 || !(occursin(".", dirs[1])) # no version number
+#                 VersionNumber(typemax(UInt32))
+#             else
+#                 VersionNumber(dirs[1])
+#             end
+
+#             # for i in 1
+#     end
+# end
+
+# Implement the RasterDataSources.jl interface.
+# With this, you can call e.g. 
+# Rasters.Raster(NaturalEarthRaster{:latest, 10, :HYP_HR}, nothing)
+
+struct NaturalEarthRaster{Version, Scale, Name} <: RasterDataSources.RasterDataSource end
+
+ne_raster(; version = nothing, scale::Int = 10, name::Int) = NaturalEarthRaster{version == nothing ? Symbol(VersionNumber(typemax(UInt32))), scale, Symbol(name)}
+
+const _NATURALEARTH_URI = RasterDataSources.URIs.URI(scheme="https", host="naturalearth.s3.amazonaws.com", path="/")
+
+function _zippath(::Type{NaturalEarthRaster{Version, Scale, Name}}) where {Version, Scale, Name}
+    return if Version == :latest || VersionNumber(string(Version)).major == typemax(UInt32)
+        "" # max version, so latest
+    else
+        string(Version) * "/"
+    end * if Scale == 110 || Scale == 50 || Scale == 10
+        string(Scale) * "m_raster"
+    else
+        error("Invalid scale: $Scale")
+    end * "/" * string(Name) * ".zip"
 end
+
+function RasterDataSources.zippath(::Type{NaturalEarthRaster{Version, Scale, Name}}) where {Version, Scale, Name}
+    return joinpath(RasterDataSources.rasterpath(), "NaturalEarth", "zips", split(_zippath(NaturalEarthRaster{Version, Scale, Name}), "/")...)
+end
+
+function RasterDataSources.zipurl(::Type{NaturalEarthRaster{Version, Scale, Name}}) where {Version, Scale, Name}
+    return RasterDataSources.URIs.URI(_NATURALEARTH_URI; path = "/" * _zippath(NaturalEarthRaster{Version, Scale, Name}))
+end
+
+function RasterDataSources.zipname(::Type{NaturalEarthRaster{Version, Scale, Name}}) where {Version, Scale, Name}
+    return splitpath(_zippath(NaturalEarthRaster{Version, Scale, Name}))[end]
+end
+
+function RasterDataSources.rasterpath(T::Type{NaturalEarthRaster{Version, Scale, Name}}) where {Version, Scale, Name}
+    return joinpath(RasterDataSources.rasterpath(), "NaturalEarth", split((_zippath(NaturalEarthRaster{Version, Scale, Name})), "/")[1:end-1]..., RasterDataSources.rastername(T))
+end
+
+function RasterDataSources.rastername(::Type{NaturalEarthRaster{Version, Scale, Name}}) where {Version, Scale, Name}
+    return string(Name) * ".tif"
+end
+
+function RasterDataSources.getraster(T::Type{NaturalEarthRaster{Version, Scale, Name}}, layer = nothing) where {Version, Scale, Name}
+    raster_path = RasterDataSources.rasterpath(T)
+    if !isfile(raster_path)
+        zip_path = RasterDataSources.zippath(T)
+        RasterDataSources._maybe_download(RasterDataSources.zipurl(T), zip_path)
+        zf = RasterDataSources.ZipFile.Reader(zip_path)
+        mkpath(dirname(raster_path))
+        raster_name = RasterDataSources.rastername(T)
+        @show zf
+        write(raster_path, read(RasterDataSources._zipfile_to_read(joinpath(splitext(raster_name)[1], raster_name), zf)))
+        close(zf)
+    end
+    return raster_path
+end
+
+
 
 function _unpack_zip(zipfile, outputdir)
     out = Pipe()
@@ -108,4 +187,91 @@ function __init__()
     global download_cache = Scratch.get_scratch!(@__MODULE__, "rasters")
 end
 
-end  # end module
+# Raster API - hook into RasterDataSources
+
+function check_scale(scale::Int)
+    return scale in (110, 50, 10)
+end
+
+function ne_file_name(scale::Int, type::String, category::String)
+    if type in (
+        "countries",
+        "map_units",
+        "map_subunits",
+        "sovereignty",
+        "tiny_countries",
+        "boundary_lines_land",
+        "pacific_groupings",
+        "breakaway_disputed_areas",
+        "boundary_lines_disputed_areas",
+        "boundary_lines_maritime_indicator")
+        type = "admin_0_" * type
+    elseif type == "states"
+        type = "admin_1_" * type
+    end
+
+    
+    if category == "raster"
+        return type
+    else
+        return "ne_$(scale)m_$(type)"
+    end
+end
+
+function(scale = 110,
+#                          type = "countries",
+#                          category = c("cultural", "physical", "raster"),
+#                          full_url = FALSE) {
+#   # check on permitted scales, convert names to numeric
+#   scale <- check_scale(scale)
+
+#   # check permitted category
+#   category <- match_arg(category)
+
+
+#   # add admin_0 to known types
+#   if (type %in% c(
+#     "countries",
+#     "map_units",
+#     "map_subunits",
+#     "sovereignty",
+#     "tiny_countries",
+#     "boundary_lines_land",
+#     "pacific_groupings",
+#     "breakaway_disputed_areas",
+#     "boundary_lines_disputed_areas",
+#     "boundary_lines_maritime_indicator"
+#   )) {
+#     type <- paste0("admin_0_", type)
+#   }
+
+
+#   # add admin_1 to known types
+#   # this actually just expands 'states' to the name including lakes
+#   if (type == "states") {
+#     type <- "admin_1_states_provinces_lakes"
+#   }
+
+
+#   if (category == "raster") {
+#     # raster seems not to have so straightforward naming, so require that name
+#     # is passed in type
+#     file_name <- paste0(type)
+#   } else {
+#     file_name <- paste0("ne_", scale, "m_", type)
+#   }
+
+
+#   # https://naturalearth.s3.amazonaws.com/110m_cultural/ne_110m_admin_0_countries.zip
+#   if (full_url) {
+#     file_name <- paste0(
+#       "https://naturalearth.s3.amazonaws.com/",
+#       scale, "m_", category, "/",
+#       file_name, ".zip"
+#     )
+#   }
+
+#   return(file_name)
+# }
+
+# end  # end module
