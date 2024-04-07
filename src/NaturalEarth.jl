bc21ff8bdebe9823e104c85d0c3e7ce6ae45a822
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-04-06 20:07:24 -0400

    Switch from Artifacts to Scratchspaces
    since we would need a builder repo to create tarballs.

--- src/NaturalEarth.jl
+++ src/NaturalEarth.jl
@@ -1,12 +1,15 @@
 module NaturalEarth
 
 import GeoJSON
-using Pkg
-using Pkg.Artifacts
+using Scratch
 
-const available_artifacts = collect(keys(
-    Artifacts.select_downloadable_artifacts(Artifacts.find_artifacts_toml(@__FILE__); include_lazy=true)
-))
+const naturalearth_cache = Ref{String}("")
+
+function __init__()
+    global naturalearth_cache
+
+    naturalearth_cache[] = @get_scratch!("naturalearth")
+end
 
 export naturalearth, bathymetry
 
@@ -18,12 +21,29 @@ Load a NaturalEarth dataset as a `GeoJSON.FeatureCollection` object.
 Valid names are found in `Artifacts.toml`. 
 We aim to support all datasets listed in https://github.com/nvkelso/natural-earth-vector/tree/master/geojson
 """
-function naturalearth(name::String)
-    pth = @artifact_str("$name/$name.geojson")
-    @assert isfile(pth) "`$name` is not a valid NaturalEarth.jl artifact"
-    GeoJSON.read(read(pth, String))
+function naturalearth(full_name::String)
+    filename = "ne_" * full_name * ".geojson"
+    if isfile(joinpath(naturalearth_cache[], filename))
+        return GeoJSON.read(read(joinpath(naturalearth_cache[], filename), String))
+    else
+        try
+            download("https://rawcdn.githack.com/nvkelso/natural-earth-vector/v5.1.2/geojson/$filename", joinpath(naturalearth_cache[], filename))
+            return GeoJSON.read(read(joinpath(naturalearth_cache[], filename), String))
+        catch e
+            if e isa RequestError
+                error("Could not download file $filename. Check the name and try again.")
+            else
+                rethrow(e)
+            end
+        end
+    end
 end
 
+function naturalearth(name::String, scale::Int)
+    @assert scale in (10, 50, 110) "`scale` must be one of 10, 50, or 110.  Got $scale."
+
+    return naturalearth("$(scale)m_$(name)")
+end
 
 """
     bathymetry(contour::Int = 2000)
@@ -34,7 +54,6 @@ The function returns a MultiPolygon describing the bathymetry at a given depth c
 The following depths should be available: [10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000, 1000, 200, 0]
 """
 function bathymetry(contour::Int=2000)
-    global available_artifacts
     # extract a list of all available bathymetry files
     bathyfiles = filter(contains("bathymetry"), available_artifacts)
     # Extract depth signifier from filename
@@ -49,6 +68,11 @@ function bathymetry(contour::Int=2000)
     return naturalearth(bathyfile)
 end
 
+function _download_url(name, scale)
+    return "https://rawcdn.githack.com/nvkelso/natural-earth-vector/v5.1.2/geojson/$(geojson_file_name(name, scale))"
+end
+
+geojson_file_name(name, scale) = "ne_$(scale)m_$(name).geojson"
 
 
 end  # end module
